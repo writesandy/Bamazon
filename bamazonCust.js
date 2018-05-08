d636dc188347a6a1c0ed367124651abb3b581ed2
Sandra Peterson <writesandy@gmail.com>
2018-05-07 19:13:34 -0500

    shop bamazon logic

--- bamazonCust.js
+++ bamazonCust.js
@@ -1,12 +1,12 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: "Acumed09",
+    password: "",
     database: "bamazon_DB"
 });
 
@@ -14,7 +14,7 @@ function start() {
     connection.query('SELECT * FROM Products', function (err,res){
         if (err) throw err;
 
-        console.log("~~~~~~~~~~~~~~Shop Bamazon!~~~~~~~~~~~~~~")
+        console.log("~~~~~~~~~~~~~~Shop Bamazon!~~~~~~~~~~~~~~");
 
         for(var i = 0; i < res.length; i++){
             console.log("ID: " +res[i].Item_ID + " | " + "Product: " + res[i].Product_Name + " | " + "Department: " + res[i].Department_Name + " | " + "Price: " +res[i].Price + " | " + "Quantity: " +res[i].Stock_Quantity);
@@ -26,7 +26,7 @@ function start() {
                 name: "id",
                 message: "What is the ID of the product you would like to purchase?",
                 validate: function(value) {
-                    if(isNAN(value) === valse && parseInt(value) <= res.length && parseInt(value) > 0) {
+                    if(isNaN(value) === false && parseInt(value) <= res.length && parseInt(value) > 0) {
                         return true;
                     } else {
                         return false;
@@ -53,17 +53,56 @@ function start() {
              if(res[whatBuying].StockQuantity >= howMany) {
                  connection.query("UPDATE Products SET ? WHERE ?", [
                      {StockQuantity: (res[whatBuying].StockQuantity - howMany)},
-                     {Item_ID: ans.id}]
-                 
-                 ], function(err,result) {
+                     {Item_ID: ans.id}
+                    
+                ], function(err,result) {
                      if(err) throw err;
                      console.log("Purchase Complete! Your total is $" +grandTotal.toFixed(2) + ". Your item(s) will be shipped to you for delivery tomorrow.")
-                 })
-             }
-
-    }
+                 });
 
 
-    )};
+                 connection.query("SELECT * FROM Departments", function (err, deptRes) {
+                     if (err) throw err;
+                     let index;
+                     for(let i = 0; i < deptRes.length; i++){
+                         if(deptRes[i].Department_Name === res[whatBuying].DepartmentName) {
+                             index = i;
+                         }
+                     }
+
+
+                 connection.query("UPDATE Departments SET ? WHERE ?", [
+                     {TotalSales: deptRes[index].TotalSales + grandTotal},
+                    {Department_Name: res[whatBuying].Department_Name}
+                 ], function (err, deptRes) {
+                     if (err) throw err;
+                 });
+            });
+
+                } else {
+            console.log("sorry, there's not enough in stock!");
+        }
+
+            askAgain();
+
+    });
+
+});
+
+}
+
+function askAgain(){
+    inquirer.prompt([{
+        type: "confirm",
+        name: "reply",
+        message: "Would you like to purchase something else?"
+    }]).then(function(ans){
+        if(ans.reply) {
+            start();
+        } else {
+            console.log("Thank you for your purchase!");
+        }
+    });
+}
 
     start();
